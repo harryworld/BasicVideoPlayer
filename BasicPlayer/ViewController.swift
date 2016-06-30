fc44ce4076a21619f8f9eaf4d89349585215cdc1
Harry Ng <harryworld@gmail.com>
2016-06-30 21:41:34 +0800

    Load video from remote location

--- BasicPlayer/ViewController.swift
+++ BasicPlayer/ViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import Cocoa
+import AVKit
+import AVFoundation
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var playerView: AVPlayerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        guard let url = NSURL(string: "https://devimages.apple.com.edgekey.net/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8") else {
+            return
+        }
+        
+        let player = AVPlayer(URL: url)
+        playerView.player = player
     }
 
     override var representedObject: AnyObject? {
